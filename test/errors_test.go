a5c5561237eb4eded2177036d000253b385d6980
StAR <zhiwen.tan@outlook.com>
2022-04-03 19:43:02 +0800

    Adjust the unit test case

--- test/errors_test.go
+++ test/errors_test.go
@@ -3,10 +3,10 @@ package test
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/nextf/errors"
-	pkgerr "github.com/pkg/errors"
 )
 
 var ErrNotFoundPage = errors.CodErr("NOT_FOUND", "Not found page")
@@ -36,31 +36,23 @@ func TestMatch(t *testing.T) {
 }
 
 func TestFormat(t *testing.T) {
-	fmt.Printf("q=%q\n----------------\n", ErrNotFoundPage)
 	if fmt.Sprintf("%q", ErrNotFoundPage) != "\"Not found page\"" {
 		t.Errorf("Expect %q, got %q", "Not found page", ErrNotFoundPage)
 	}
-	fmt.Printf("s=%s\n----------------\n", ErrNotFoundPage)
 	if fmt.Sprintf("%s", ErrNotFoundPage) != "Not found page" {
 		t.Errorf("Expect %s, got %s", "Not found page", ErrNotFoundPage)
 	}
-	fmt.Printf("v=%v\n----------------\n", ErrNotFoundPage)
 	if fmt.Sprintf("%v", ErrNotFoundPage) != "[NOT_FOUND] Not found page" {
 		t.Errorf("Expect %v, got %v", "Not found page", ErrNotFoundPage)
 	}
-	fmt.Printf("+v=%+v\n----------------\n", ErrNotFoundPage)
-	// withStack := fmt.Sprintf("%+v", ErrNotFoundPage)
+	if fmt.Sprintf("%+v", ErrNotFoundPage) != "[NOT_FOUND] Not found page" {
+		t.Errorf("Expect %+v, got %+v", "Not found page", ErrNotFoundPage)
+	}
 
 	e1 := errors.Wrap(ErrNotFoundPage, "Not found index.html")
-	fmt.Printf("q=%q\n----------------\n", e1)
-	fmt.Printf("s=%s\n----------------\n", e1)
-	fmt.Printf("v=%v\n----------------\n", e1)
-	fmt.Printf("+v=%+v\n----------------\n", e1)
-}
-
-func TestStacker(t *testing.T) {
-	stack := errors.Wrap(ErrEndOfStream, "test")
-	if x, ok := stack.(interface{ StackTrace() pkgerr.StackTrace }); ok {
-		fmt.Printf("%+v\n", x.StackTrace())
+	withStack := fmt.Sprintf("%+v", e1)
+	logPrefix := "[NOT_FOUND] Not found page\nNot found index.html\ngithub.com/nextf/errors.WithStack"
+	if !strings.HasPrefix(withStack, logPrefix) {
+		t.Error("Unprinted stack.")
 	}
 }
