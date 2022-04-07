f57be0349f8d5feb1abcb40c97fda07e783bf31e
StAR <zhiwen.tan@outlook.com>
2022-04-07 23:22:23 +0800

    Some unit test cases were added

--- functions_test.go
+++ functions_test.go
@@ -2,6 +2,7 @@ package errors_test
 
 import (
 	"fmt"
+	"math/rand"
 	"reflect"
 	"strings"
 	"testing"
@@ -10,10 +11,9 @@ import (
 )
 
 var ErrNotFoundPage = errors.New("NOT_FOUND", "Not found page")
-var ErrNotFoundFile = errors.New("NOT_FOUND", "Not found file")
 var ErrEndOfStream = errors.New("EOF", "End of stream")
 
-func TestAs(t *testing.T) {
+func TestErrCodeAs(t *testing.T) {
 	e1 := errors.WithMessage(ErrNotFoundPage, "Not found index.html")
 	var coder interface{ Code() string }
 	if errors.As(e1, &coder) {
@@ -35,7 +35,7 @@ func TestMatch(t *testing.T) {
 	}
 }
 
-func TestFormat(t *testing.T) {
+func TestErrCodeFormat(t *testing.T) {
 	if fmt.Sprintf("%q", ErrNotFoundPage) != "\"Not found page\"" {
 		t.Errorf("Expect %q, got %q", "Not found page", ErrNotFoundPage)
 	}
@@ -51,7 +51,7 @@ func TestFormat(t *testing.T) {
 
 	e1 := errors.WithMessage(ErrNotFoundPage, "Not found index.html")
 	withStack := fmt.Sprintf("%+v", e1)
-	logPrefix := "[NOT_FOUND] Not found page\nNot found index.html\ngithub.com/nextf/errors_test.TestFormat\n"
+	logPrefix := "[NOT_FOUND] Not found page\nNot found index.html\ngithub.com/nextf/errors_test.TestErrCodeFormat\n"
 	if !strings.HasPrefix(withStack, logPrefix) {
 		t.Error("Unprinted stack.")
 	}
@@ -129,5 +129,59 @@ func TestWithMessagef(t *testing.T) {
 	}
 	if level != times+1 {
 		t.Errorf("Expect %d, got %d", times+1, level)
+	}
+}
+
+func TestNew(t *testing.T) {
+	err := errors.New("NEW_IN_FUNC", "error for unittest")
+	if errors.HasStackTrace(err) {
+		t.Errorf("It was expected that there would be no StackTrace in the `err`, but it wasn't.")
+	}
+	if code, ok := errors.GetCode(err); !ok || code != "NEW_IN_FUNC" {
+		t.Errorf("Expect code=%s, got [%s]", "NEW_IN_FUNC", code)
+	}
+	if err.Error() != "error for unittest" {
+		t.Errorf("Expect `%s`, got `%s`", "error for unittest", err.Error())
+	}
+}
+
+func TestNewWithStack(t *testing.T) {
+	err := errors.NewWithStack("NEW_IN_FUNC", "error for unittest")
+	if !errors.HasStackTrace(err) {
+		t.Errorf("It was expected that there would has StackTrace in the `err`, but it wasn't.")
+	}
+	if code, ok := errors.GetCode(err); !ok || code != "NEW_IN_FUNC" {
+		t.Errorf("Expect code=%s, got [%s]", "NEW_IN_FUNC", code)
+	}
+	if err.Error() != "error for unittest" {
+		t.Errorf("Expect `%s`, got `%s`", "error for unittest", err.Error())
+	}
+}
+
+func TestNewf(t *testing.T) {
+	num := rand.Int()
+	err := errors.Newf("NEW_IN_FUNC", "Wrong number [%d]", num)
+	if errors.HasStackTrace(err) {
+		t.Errorf("It was expected that there would be no StackTrace in the `err`, but it wasn't.")
+	}
+	if code, ok := errors.GetCode(err); !ok || code != "NEW_IN_FUNC" {
+		t.Errorf("Expect code=%s, got [%s]", "NEW_IN_FUNC", code)
+	}
+	if err.Error() != fmt.Sprintf("Wrong number [%d]", num) {
+		t.Errorf("Expect `%s`, got `%s`", fmt.Sprintf("Wrong number [%d]", num), err.Error())
+	}
+}
+
+func TestNewWithStackf(t *testing.T) {
+	num := rand.Int()
+	err := errors.NewWithStackf("NEW_IN_FUNC", "Wrong number [%d]", num)
+	if !errors.HasStackTrace(err) {
+		t.Errorf("It was expected that there would has StackTrace in the `err`, but it wasn't.")
+	}
+	if code, ok := errors.GetCode(err); !ok || code != "NEW_IN_FUNC" {
+		t.Errorf("Expect code=%s, got [%s]", "NEW_IN_FUNC", code)
+	}
+	if err.Error() != fmt.Sprintf("Wrong number [%d]", num) {
+		t.Errorf("Expect `%s`, got `%s`", fmt.Sprintf("Wrong number [%d]", num), err.Error())
 	}
 }
