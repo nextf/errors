951fa368c13740c53362a8eec549dd46fec3cd1e
StAR <zhiwen.tan@outlook.com>
2022-04-23 15:37:36 +0800

    Add unit test cases

--- errconst_test.go
+++ errconst_test.go
@@ -7,22 +7,28 @@ import (
 )
 
 const (
-	errNoCode      = errors.ConstError("No code")
-	errInvalidCode = errors.ConstError("[ERR 001] This error has a invalid code")
-	errHasCode     = errors.ConstError("[ERR001] This error has a code ")
+	errNoCode        = errors.ConstError("No code")
+	errInvalidCode   = errors.ConstError("[ERR 001] This error has a invalid code")
+	errNormalize     = errors.ConstError("[ERR001] This error has a code")
+	errAbnormality   = errors.ConstError("\t[ERR001]   This error has a code  ")
+	errHasSeparator1 = errors.ConstError("[ERR-001] This error has a code")
+	errHasSeparator2 = errors.ConstError("[ERR_001] This error has a code")
 )
 
-func TestConst(t *testing.T) {
+func TestConstError(t *testing.T) {
 	var err error = errNoCode
 	code, ok := errors.GetCode(err)
 	if !ok {
 		t.Errorf("No code in ConstError")
 	}
 	if code != "" {
-		t.Errorf("Get code by accident")
+		t.Errorf("Get unexpected code")
 	}
 	if err.Error() != "No code" {
 		t.Errorf("Unexpected error message")
+	}
+	if !errors.Match(err, "") {
+		t.Errorf("Get unexpected code")
 	}
 
 	err = errInvalidCode
@@ -36,8 +42,11 @@ func TestConst(t *testing.T) {
 	if err.Error() != "[ERR 001] This error has a invalid code" {
 		t.Errorf("Unexpected error message")
 	}
+	if !errors.Match(err, "") {
+		t.Errorf("Get unexpected code")
+	}
 
-	err = errHasCode
+	err = errNormalize
 	code, ok = errors.GetCode(err)
 	if !ok {
 		t.Errorf("No code in ConstError")
@@ -47,5 +56,53 @@ func TestConst(t *testing.T) {
 	}
 	if err.Error() != "This error has a code" {
 		t.Errorf("Unexpected error message")
+	}
+	if !errors.Match(err, "ERR001") {
+		t.Errorf("Get unexpected code")
+	}
+
+	err = errAbnormality
+	code, ok = errors.GetCode(err)
+	if !ok {
+		t.Errorf("No code in ConstError")
+	}
+	if code != "ERR001" {
+		t.Errorf("Get unexpected code")
+	}
+	if err.Error() != "This error has a code" {
+		t.Errorf("Unexpected error message")
+	}
+	if !errors.Match(err, "ERR001") {
+		t.Errorf("Get unexpected code")
+	}
+
+	err = errHasSeparator1
+	code, ok = errors.GetCode(err)
+	if !ok {
+		t.Errorf("No code in ConstError")
+	}
+	if code != "ERR-001" {
+		t.Errorf("Get unexpected code")
+	}
+	if err.Error() != "This error has a code" {
+		t.Errorf("Unexpected error message")
+	}
+	if !errors.Match(err, "ERR-001") {
+		t.Errorf("Get unexpected code")
+	}
+
+	err = errHasSeparator2
+	code, ok = errors.GetCode(err)
+	if !ok {
+		t.Errorf("No code in ConstError")
+	}
+	if code != "ERR_001" {
+		t.Errorf("Get unexpected code")
+	}
+	if err.Error() != "This error has a code" {
+		t.Errorf("Unexpected error message")
+	}
+	if !errors.Match(err, "ERR_001") {
+		t.Errorf("Get unexpected code")
 	}
 }
